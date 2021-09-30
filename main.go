f373f393e4e41c44a368f6a7bba958cfb147d621
Pietro Atzeni <pietrino.atzeni@gmail.com>
2021-09-30 09:00:59 +0200

    Added REPL

--- main.go
+++ main.go
@@ -1,6 +1,24 @@
-package monkey
+package main
 
 import (
+	"fmt"
 	_ "monkey/lexer"
+	"monkey/repl"
 	_ "monkey/token"
+	"os"
+	"os/user"
 )
+
+func main() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+
+	fmt.Printf("Feel free to type in commands\n")
+
+	repl.Start(os.Stdin, os.Stdout)
+}
