62b147308861900cb65628abb65272fd0d695488
Pietro Atzeni <pietrino.atzeni@moneyfarm.com>
2021-10-15 16:03:05 +0200

    Added evaluation for return statements

--- object/object.go
+++ object/object.go
@@ -5,9 +5,10 @@ import "fmt"
 type ObjectType string
 
 const (
-	BOOLEAN_OBJ = "BOOLEAN"
-	INTEGER_OBJ = "INTEGER"
-	NULL_OBJ    = "NULL"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	INTEGER_OBJ      = "INTEGER"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 type Object interface {
@@ -44,4 +45,15 @@ func (n *Null) Inspect() string {
 }
 func (n *Null) Type() ObjectType {
 	return NULL_OBJ
+}
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Inspect() string {
+	return rv.Value.Inspect()
+}
+func (rv *ReturnValue) Type() ObjectType {
+	return RETURN_VALUE_OBJ
 }
